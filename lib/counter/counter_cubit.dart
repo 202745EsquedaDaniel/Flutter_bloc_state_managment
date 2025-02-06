7cd1c0c2f157a4e9c68a3e9bb54517d982bb3e77
eduardani84 <eduardani84@gmail.com>
2025-02-06 17:26:24 +0000

    [add] - cubit

---
+++ lib/counter/counter_cubit.dart
@@ -0,0 +1,20 @@
+/*
+
+CUBIT: Simplified version of bloc for easy state managment
+
+Let's create our own counter cubit
+
+*/
+
+import 'package:bloc/bloc.dart';
+
+class CounterCubit extends Cubit<int> {
+  //  constructor: get initial state
+  CounterCubit(super.initialState);
+
+  //  increment
+  void increment() => emit(state + 1);
+
+  //  decrement
+  void decrement() => emit(state - 1);
+}
